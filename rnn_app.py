ba3c7b5c04f05c7b1acd4c7fc367acd4729b1a0d
Radheya Ketak <136088677+Rketak@users.noreply.github.com>
2025-03-06 17:37:29 +0530

    file name change

--- rnn_app.py
+++ rnn_app.py
@@ -10,7 +10,7 @@ word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 
 # Load pre-trained model
-model = load_model('simple_rnn_imdb.keras')
+model = load_model('simple_RNN_imdb.keras')
 
 # Function to preprocess text
 def preprocess_text(text):
